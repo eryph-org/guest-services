f105918c73ffd5e238316a47931ad55e7bd10c48
Christopher Mann <christopher.mann@dbosoft.eu>
2025-07-29 17:31:59 +0200

    Improve pipeline and install script

--- packaging/iso/install.ps1
+++ packaging/iso/install.ps1
@@ -4,8 +4,8 @@
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 3.0
 
-Expand-Archive -Path $PSScriptRoot\*.zip -DestinationPath "C:\Program Files\eryph\guest-services"
+Expand-Archive -Path $PSScriptRoot\*.zip -DestinationPath "C:\Program Files\eryph\guest-services" -ProgressAction SilentlyContinue
 
-sc.exe create eryph-guest-services binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
-sc.exe failure eryph-guest-services reset=60 actions=restart/10000
-sc.exe start eryph-guest-services
+$null = sc.exe create eryph-guest-services binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
+$null = sc.exe failure eryph-guest-services reset=60 actions=restart/10000
+$null = sc.exe start eryph-guest-services
