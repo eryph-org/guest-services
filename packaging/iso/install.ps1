38f8328a3a2104f74a25882deb2fa92c45420a57
Christopher Mann <christopher.mann@dbosoft.eu>
2025-08-14 15:29:26 +0200

    Enable auto start for egs service on Windows

--- packaging/iso/install.ps1
+++ packaging/iso/install.ps1
@@ -6,6 +6,6 @@ Set-StrictMode -Version 3.0
 
 Expand-Archive -Path $PSScriptRoot\*.zip -DestinationPath "C:\Program Files\eryph\guest-services" -ProgressAction SilentlyContinue
 
-$null = sc.exe create eryph-guest-services binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
+$null = sc.exe create eryph-guest-services start=auto binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
 $null = sc.exe failure eryph-guest-services reset=60 actions=restart/10000
 $null = sc.exe start eryph-guest-services
