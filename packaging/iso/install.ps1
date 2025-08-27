5f157b8e562038caa5bd5addf40cfca5a3101e5d
Frank Wagner <frank2568@gmail.com>
2025-08-27 11:17:32 +0200

    add service update support
    Added support for re-running the script if service is already installed.

--- packaging/iso/install.ps1
+++ packaging/iso/install.ps1
@@ -8,11 +8,22 @@ $savedProgressPreference = $global:ProgressPreference
 
 try {
     $global:ProgressPreference = 'SilentlyContinue'
+
+    $service = Get-Service eryph-guest-services -ErrorAction SilentlyContinue
+    if($service) {
+        $service | Stop-Service
+        Remove-Item -Recurse "C:\Program Files\eryph\guest-services"
+    }
+
     Expand-Archive -Path $PSScriptRoot\*.zip -DestinationPath "C:\Program Files\eryph\guest-services"
 
-    $null = sc.exe create eryph-guest-services start=auto binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
-    $null = sc.exe failure eryph-guest-services reset=60 actions=restart/10000
-    $null = sc.exe start eryph-guest-services
+    if(-not $service) {
+        $null = sc.exe create eryph-guest-services start=auto binpath="C:\Program Files\eryph\guest-services\bin\egs-service.exe"
+        $null = sc.exe failure eryph-guest-services reset=60 actions=restart/10000
+        $null = sc.exe start eryph-guest-services
+    } else{
+        $service | Start-Service
+    }
 } finally {
     $global:ProgressPreference = $savedProgressPreference
 }
